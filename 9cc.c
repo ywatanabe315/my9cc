179bfb8938fe252c619c3ab23466079881f9eda9
ywatanabe315 <nabeo.dendai.master31@gmail.com>
2024-03-24 18:16:06 +0900

    add error_at

--- 9cc.c
+++ 9cc.c
@@ -30,6 +30,21 @@ void error(char *fmt, ...) {
 	exit(1);
 }
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 bool consume(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op)
 		return false;
@@ -39,13 +54,13 @@ bool consume(char op) {
 
 void expect(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op)
-		error("not '%c'", op);
+		error_at(token->str, "not '%c'", op);
 	token = token->next;
 }
 
 int expect_number() {
 	if (token->kind != TK_NUM)
-		error("not number");
+		error_at(token->str, "not number");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -63,7 +78,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 	return tok;
 }
 
-Token *tokenize(char *p) {
+Token *tokenize() {
+	char *p = user_input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -85,7 +101,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		error("can not tokenize");
+		error_at(p, "can not tokenize");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -98,7 +114,8 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
